e7c87c81f863e71cc744da8b48c5b6eaa6f97d4e
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-10 10:36:25 -0400

    set up server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
-// build your server here and require it from index.js
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
+module.exports = server;
